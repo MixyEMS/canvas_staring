bf7211b08e6d4071d9b92ed57960c278ed4073e9
Mixy <835410991@qq.com>
2017-08-01 22:34:23 +0800

    额  一个canvas小动画，自己优化整理了一点

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var can;
 var ctx;
 
 var w;
-var h ;
+var h;
 
 var girlPic = new Image();
 var starPic = new Image();
@@ -10,42 +10,89 @@ var starPic = new Image();
 var num = 60;
 var stars = [];
 
-function init(){
-	can = document.getElementById("canvas");
-	ctx = can.getContext("2d");
-    
+var lastTime;
+var detalTime;
+
+var switchy = false;
+var life = 0;
+
+function init() {
+    can = document.getElementById("canvas");
+    ctx = can.getContext("2d");
+
     w = can.width;
     h = can.height;
 
-    girlPic.src="src/girl.jpg";
+    document.getElementById("canvas").addEventListener("mousemove", mousemove);
+
+    girlPic.src = "src/girl.jpg";
     starPic.src = "src/star.png";
 
-    for(var i = 0 ; i< num ;i++){
-    	var obj = new starObj();
-    	obj.init();
-    	stars.push(obj);
+    for (var i = 0; i < num; i++) {
+        var obj = new starObj();
+        obj.init();
+        stars.push(obj);
     }
 
-	gameloop();
+    lastTime = Date.now();
+
+    gameloop();
 }
 
 document.body.onload = init;
 
-function gameloop(){
+function gameloop() {
 
-	window.requestAnimationFrame(gameloop);
-	drawBackground();
-	drawGirl();
-	drawStar();
-   
+    window.requestAnimationFrame(gameloop);
+    var now = Date.now();
+    detalTime = now - lastTime;
+    lastTime = now;
+
+    drawBackground();
+    drawGirl();
+    aliveUpdate();
+    drawStar(detalTime);
+
 }
 
-function drawBackground(){
+function drawBackground() {
 
-	ctx.fillStyle="#333";
-	ctx.fillRect(0,0,w,h);
+    ctx.fillStyle = "#333";
+    ctx.fillRect(0, 0, w, h);
 }
 
-function drawGirl(){
-	 ctx.drawImage(girlPic,100,100,600,400);
+function drawGirl() {
+    ctx.drawImage(girlPic, 100, 100, 600, 400);
+}
+
+function drawStar(detalTime) {
+    ctx.save();
+    ctx.globalAlpha = life;
+    for (var i = 0; i < num; i++) {
+        stars[i].update(detalTime);
+    }
+    ctx.restore();
+}
+
+function aliveUpdate() {
+    if (switchy) {
+        life += 0.03 * detalTime * 0.05;
+        life = life > 1 ? 1 : life;
+    } else {
+        life -= 0.03 * detalTime * 0.05;
+        life = life < 0 ? 0 : life;
+    }
+}
+
+function mousemove(e) {
+    if (e.offsetX || e.layerX) {
+        var px = e.offsetX == undefined ? e.layerX : e.offsetX;
+        var py = e.offsetY == undefined ? e.layerY : e.offsetY;
+
+        if (px >= 100 && px <= 700 && py >= 100 && py <= 500) {
+            switchy = true;
+        } else {
+            switchy = false;
+        }
+    }
 }
--- js/star.js
+++ js/star.js
@@ -1,18 +1,41 @@
-var starObj = function(){
-	this.x;
-	this.y;
+var starObj = function() {
+    this.x;
+    this.y;
+    this.picNo;
+    this.timer;
+
+    this.xSpd;
+    this.ySpd;
 }
 
-starObj.prototype.init = function(){
-	 this.x = Math.random()*600 + 100;
-	 this.y = Math.random()*400 + 100;
+starObj.prototype.init = function() {
+    this.x = Math.random() * 600 + 100;
+    this.y = Math.random() * 400 + 100;
+    this.picNo = Math.floor(Math.random() * 7);
+    this.timer = 0;
+    this.xSpd = Math.random() * 0.6 - 0.3;
+    this.ySpd = Math.random() * 0.6 - 0.3;
 }
 
-starObj.prototype.draw = function(){
-	ctx.drawImage(starPic,this.y,this.y);
+starObj.prototype.update = function(detalTime) {
+
+    this.timer += detalTime;
+    if (this.timer > 50) {
+        this.picNo = (this.picNo+1)%7;
+        this.timer = 0;
+        this.x += this.xSpd;
+        this.y += this.ySpd;
+
+        if (this.x >= 700-7 || this.y >= 500-7 || this.x <= 100 || this.y <= 100) {
+            this.init();
+            return;
+        }
+    }
+
+    this.draw();
+
 }
-function drawStar(){
-	for(var i = 0 ; i < num ; i++){
-		 stars[i].draw();
-	}
+
+starObj.prototype.draw = function() {
+    ctx.drawImage(starPic, 7 * this.picNo, 0, 7, 7, this.x, this.y, 7, 7);
 }
